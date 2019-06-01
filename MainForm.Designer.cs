7dc36c2d646d6040e9e47f956ceb14819f4a4d82
Sebastian S. Cocioba <sebastian@binomicalabs.org>
2019-06-01 18:39:13 -0400

    Added a main menu to call up modules

---
+++ MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Binomics_Labs_Software_Suite
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnIDM = new System.Windows.Forms.Button();
+            this.btnCodonBiasAnalyzer = new System.Windows.Forms.Button();
+            this.btnDNAVisualizationAbstractor = new System.Windows.Forms.Button();
+            this.btnBacterialMiniSpecController = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIDM
+            // 
+            this.btnIDM.Location = new System.Drawing.Point(12, 12);
+            this.btnIDM.Name = "btnIDM";
+            this.btnIDM.Size = new System.Drawing.Size(552, 88);
+            this.btnIDM.TabIndex = 0;
+            this.btnIDM.Text = "Infinite Discovery Machine Simulator";
+            this.btnIDM.UseVisualStyleBackColor = true;
+            this.btnIDM.Click += new System.EventHandler(this.BtnIDM_Click);
+            // 
+            // btnCodonBiasAnalyzer
+            // 
+            this.btnCodonBiasAnalyzer.Location = new System.Drawing.Point(12, 106);
+            this.btnCodonBiasAnalyzer.Name = "btnCodonBiasAnalyzer";
+            this.btnCodonBiasAnalyzer.Size = new System.Drawing.Size(552, 88);
+            this.btnCodonBiasAnalyzer.TabIndex = 2;
+            this.btnCodonBiasAnalyzer.Text = "Codon Bias Analyzer";
+            this.btnCodonBiasAnalyzer.UseVisualStyleBackColor = true;
+            this.btnCodonBiasAnalyzer.Click += new System.EventHandler(this.BtnCodonBiasAnalyzer_Click);
+            // 
+            // btnDNAVisualizationAbstractor
+            // 
+            this.btnDNAVisualizationAbstractor.Location = new System.Drawing.Point(12, 294);
+            this.btnDNAVisualizationAbstractor.Name = "btnDNAVisualizationAbstractor";
+            this.btnDNAVisualizationAbstractor.Size = new System.Drawing.Size(552, 88);
+            this.btnDNAVisualizationAbstractor.TabIndex = 4;
+            this.btnDNAVisualizationAbstractor.Text = "DNA Visualization Abstractor";
+            this.btnDNAVisualizationAbstractor.UseVisualStyleBackColor = true;
+            // 
+            // btnBacterialMiniSpecController
+            // 
+            this.btnBacterialMiniSpecController.Location = new System.Drawing.Point(12, 200);
+            this.btnBacterialMiniSpecController.Name = "btnBacterialMiniSpecController";
+            this.btnBacterialMiniSpecController.Size = new System.Drawing.Size(552, 88);
+            this.btnBacterialMiniSpecController.TabIndex = 3;
+            this.btnBacterialMiniSpecController.Text = "Bacterial MiniSpec Controller";
+            this.btnBacterialMiniSpecController.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(809, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 7;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 388);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(552, 88);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(576, 489);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDNAVisualizationAbstractor);
+            this.Controls.Add(this.btnBacterialMiniSpecController);
+            this.Controls.Add(this.btnCodonBiasAnalyzer);
+            this.Controls.Add(this.btnIDM);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Binomica Labs Software Suite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIDM;
+        private System.Windows.Forms.Button btnCodonBiasAnalyzer;
+        private System.Windows.Forms.Button btnDNAVisualizationAbstractor;
+        private System.Windows.Forms.Button btnBacterialMiniSpecController;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binomics_Labs_Software_Suite
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnIDM_Click(object sender, EventArgs e)
+        {
+            InfiniteDiscoveryMachineSimulator IDM = new InfiniteDiscoveryMachineSimulator();
+            this.WindowState = FormWindowState.Minimized;
+            IDM.Show();
+            IDM.Activate();
+            IDM.FormClosed += new FormClosedEventHandler(IDM_FormClosed);
+
+           
+
+        }
+
+        void IDM_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void BtnCodonBiasAnalyzer_Click(object sender, EventArgs e)
+        {
+            CodonBiasAnalyzer CBA = new CodonBiasAnalyzer();
+            this.WindowState = FormWindowState.Minimized;
+            CBA.Show();
+            CBA.Activate();
+            CBA.FormClosed += new FormClosedEventHandler(CBA_FormClosed);
+
+        }
+
+        void CBA_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
